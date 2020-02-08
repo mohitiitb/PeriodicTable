0a2abf0a25b985383c63ebd732322b3f190feeeb
Mohit Gupta <32578141+mohitiitb@users.noreply.github.com>
2020-02-08 15:55:56 +0530

    checkbox error

--- src/index.js
+++ src/index.js
@@ -2,16 +2,58 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 import { elements } from "./_data";
-
-function checkbox(props) {}
+import Checkbox from "./checkbox";
 
 function PeriodicTable(props) {
-  var category = [{ a: 1, b: 1 }];
+  const category = {
+    noble: 0,
+    alkali: 1,
+    alkaline: 1,
+    transition: 1,
+    polyatomic: 1,
+    diatomic: 1,
+    "post-transition": 1,
+    metalloid: 1,
+    lanthanide: 1,
+    actinide: 1,
+    "unknown,": 1
+  };
+  function getFirstWord(str) {
+    let spacePosition = str.indexOf(" ");
+    if (spacePosition === -1) return str;
+    else return str.substr(0, spacePosition);
+  }
+  function handleCheckboxChange(props) {}
+  function renderCell(props) {
+    if (!category[getFirstWord(props.category)]) {
+      return (
+        <div className={`element element-${props.number} ${props.category}`} />
+      );
+    } else {
+      return Cell(props);
+    }
+  }
   return (
     <div className="wrapper">
       <h1 align="center"> The Periodic Table of Elements </h1>
-      <div id="table">{elements.map(block => Cell(block))}</div>
-      <div align="center">hello</div>
+      <div id="table">{elements.map(block => renderCell(block))}</div>
+      <div align="center">
+        {/* {Object.keys(category).map(key => category[key])} */}
+        {/* {Object.keys(category).map(key => (
+          <p>{key}</p>
+        ))} */}
+        {Object.keys(category).map(key => (
+          <label key={key}>
+            {" "}
+            {key}
+            <Checkbox
+              name={key}
+              checked={category[key]}
+              onChange={handleCheckboxChange}
+            />
+          </label>
+        ))}
+      </div>
     </div>
   );
 }
@@ -21,6 +63,7 @@ function Cell(props) {
     <div className={`element element-${props.number} ${props.category}`}>
       <div className="number">{props.number}</div>
       <div className="symbol">{props.symbol}</div>
+      <div className="element-name">{props.category}</div>
     </div>
   );
 }
