544c848593525ef65ca059ddfec8af29b2b4932a
Mohit Gupta <32578141+mohitiitb@users.noreply.github.com>
2020-02-08 12:23:01 +0530

    Added data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import "./styles.css";
+import { elements } from "./_data";
 
-import App from "./App";
+function checkbox(props) {}
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+function PeriodicTable(props) {
+  var category = [{ a: 1, b: 1 }];
+  return (
+    <div className="wrapper">
+      <h1 align="center"> The Periodic Table of Elements </h1>
+      <div id="table">{elements.map(block => Cell(block))}</div>
+      <div align="center">hello</div>
+    </div>
+  );
+}
+
+function Cell(props) {
+  return (
+    <div className={`element element-${props.number} ${props.category}`}>
+      <div className="number">{props.number}</div>
+      <div className="symbol">{props.symbol}</div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root"); //see index.html
+ReactDOM.render(<PeriodicTable />, rootElement);
